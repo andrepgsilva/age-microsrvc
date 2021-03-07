37d255a8ae80dcbe6bea40bb4a0abfd0163e41d8
Andre Silva <andrepgsilva@outlook.com>
2021-03-07 16:05:01 +0000

    Show my age

--- public/index.php
+++ public/index.php
@@ -1,1 +1,14 @@
-okokok
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$date = new \DateTime();
+$date->setDate(1984, 9, 8);
+
+$today = new \DateTime();
+
+$age = new \App\Age($date, $today);
+
+?>
+
+Minha idade é: <?php $age; ?>
